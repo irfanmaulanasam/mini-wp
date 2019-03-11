102961de5153b10212a3f07cdcee573fc2c86c25
irfan maulanasam <alternatiflab@gmail.com>
2019-03-11 22:34:03 +0700

    irfanmaulanasam

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('.editor').hide()
+})
+
+
+function listArticle() {
+    $('.container').show()
+    $('.editor').hide()
+}
+
+function addArticle() {
+    $('.container').hide()
+    $('.editor').show()
+} 
